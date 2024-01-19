71e30b97106dc48e3c5c932a3055b520dcd55711
MaliyOna <veronika.pr777@gmail.com>
2024-01-20 00:23:19 +0300

    add content

---
+++ client/src/shared/components/Content/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.scss';
+
+export function Content(props) {
+  return (
+    <div className='content'>
+      {
+        props.children
+      }
+    </div>
+  );
+}
